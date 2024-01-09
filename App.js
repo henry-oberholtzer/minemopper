443f6b076bdec15ecbfdcd9a4c8ebf9ce6c00c23
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-09 15:06:37 -0800

    fixed app

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StartPopUp from './components/StartPopUp';
 import LeaderBoard from './components/LeaderBoard';
@@ -7,13 +7,6 @@ import Board from './components/Board';
 import newGame from './game_logic/board-creator';
 
 const game = newGame(8, 8, 8);
-// import LoginForm from './auth/LoginForm';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import MainMenu from './screens/MainMenu';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
