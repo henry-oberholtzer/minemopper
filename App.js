31e60cfc8ab9d37a0f031d7a6408b56da10414bf
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-09 14:52:00 -0800

    changes to board, leaderboard, mainmenu, app

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 		<View style={styles.container}>
 			<StartPopUp />
 			<MainMenu />
-			<LeaderBoard />
+			{/* <LeaderBoard /> Keeping this comment out until scores is defined*/}
 			<StatusBar style="auto" />
 		</View>
 	);
