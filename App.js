ce4cc0507af6af28a4b0991a1d9a9d505dde9ed9
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2024-01-09 13:21:53 -0800

    Add LB, MM, SPU components

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartPopUp from './assets/components/StartPopUp';
+import LeaderBoard from './assets/components/LeaderBoard';
+import MainMenu from './assets/components/MainMenu';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+			<StartPopUp />
+			<MainMenu />
+			<LeaderBoard /> 
       <StatusBar style="auto" />
     </View>
   );
