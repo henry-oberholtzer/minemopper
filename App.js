943abd2a89e4a81d447113f89b857a3f5d0ca124
Aaron Mejia <145869330+mejia-dev@users.noreply.github.com>
2024-01-09 12:00:29 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from './auth/LoginForm';
+import { NavigationContainer } from 'react-navigation/native'
+import { createNativeStackNavigator } from 'react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator>
+      {/* <Stack.Screen name='LogIn' component={LoginForm}/> */}
+      </StackNavigator>
+    </NavigationContainer>
   );
 }
 
