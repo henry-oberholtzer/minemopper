c8073c42bdc92863ee53f407e35be0b13414bff2
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-01-10 11:23:43 -0800

    add navigation to board with difficulty

--- App.js
+++ App.js
@@ -6,25 +6,29 @@ import MainMenu from './components/MainMenu';
 import Board from './components/Board';
 import newGame from './game_logic/board-creator';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const game = newGame(8, 8, 8);
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <MainMenu />
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={MainMenu} />
+        <Stack.Screen name="Board" component={Board} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer >
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#C0C0C0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#C0C0C0',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
--- components/MainMenu.jsx
+++ components/MainMenu.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import { View, StyleSheet, Text, Image, Modal } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Button from './Button';
+import newGame from '../game_logic/board-creator';
+
+const game = newGame(8, 8, 8);
 
 
-const MainMenu = () => {
+const MainMenu = ({ navigation }) => {
     const [view, setView] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
 
@@ -24,36 +27,49 @@ const MainMenu = () => {
         setView(false);
     };
 
-	let currentView;
+    const goToBoard = (x, y, bombs) => {
+        navigation.navigate('Board', { row: x, column: y, bombsNum: bombs });
+    }
+
+    const play = (x, y, bombs) => {
+        goToBoard(x, y, bombs);
+        modalOff();
+    };
+
+    let currentView;
 
     if (!view) {
         currentView = (
             <>
-                <View style={styles.buttonContainer}>
-                    <Button label="Start Game" func={pregame} />
-                    <Button label="Resume Game" />
-                    <Button label="View Leaderboard" />
-                    <Button label="Sign out" />
+                <View style={styles.container}>
+                    <View style={styles.buttonContainer}>
+                        <Button label="Start Game" func={() => pregame()} />
+                        <Button label="Resume Game" />
+                        <Button label="View Leaderboard" />
+                        <Button label="Sign out" />
+                    </View>
                 </View>
             </>
         );
     } else {
         currentView = (
-            <View style={styles.buttonContainer}>
-                <Button label="Difficulty" func={modalOn} />
-                <Button label="Oops mode?" />
-                <Button label="Back" func={exit} theme="back-button" />
-                <Modal visible={modalVisible}
-                    onRequestClose={() => setModalVisible(false)}
-                    animationType="slide">
-                    <View style={styles.headerStyle}></View>
-                    <View style={styles.buttonContainer}>
-                        <Button label="Easy" />
-                        <Button label="Medium" />
-                        <Button label="Hard" />
-                        <Button label="Back" func={modalOff} theme="back-button" />
-                    </View>
-                </Modal>
+            <View style={styles.container}>
+                <View style={styles.buttonContainer}>
+                    <Button label="Difficulty" func={() => modalOn()} />
+                    <Button label="Oops mode?" />
+                    <Button label="Back" func={() => exit()} theme="back-button" />
+                    <Modal visible={modalVisible}
+                        onRequestClose={() => setModalVisible(false)}
+                        animationType="slide">
+                        <View style={styles.headerStyle}></View>
+                        <View style={styles.buttonContainer}>
+                            <Button label="Easy" func={() => play(8, 8, 8)} />
+                            <Button label="Medium" func={() => play(16, 16, 16)} />
+                            <Button label="Hard" func={() => play(32, 32, 32)} />
+                            <Button label="Back" func={() => modalOff()} theme="back-button" />
+                        </View>
+                    </Modal>
+                </View>
             </View>
         );
     }
@@ -110,7 +126,22 @@ const styles = StyleSheet.create({
     image: {
         width: wp('100%'),
         height: hp('30%'),
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#C0C0C0',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
 });
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#C0C0C0',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//   });
 
 export default MainMenu;
