3e3f5be15dd9e8e634261bc6161ba04deb20a72c
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-10 13:59:41 -0800

    Change declaration of Leaderboard to be a React component

--- components/LeaderBoard.tsx
+++ components/LeaderBoard.tsx
@@ -4,7 +4,7 @@ import { db } from './auth/Firebase';
 import { collection, getDocs, query } from "firebase/firestore";
 
 
-export default function LeaderBoard() {
+const LeaderBoard: React.FC = () => {
 	
 	const [highScores, setHighScores] = useState([]);
 
@@ -72,7 +72,7 @@ export default function LeaderBoard() {
 const styles = StyleSheet.create({
 	mainBodyStyle: {
 		backgroundColor: '#C0C0C0',
-		width: '100%'
+		width: '100%',
 	},
 	mainBodyTitle: {
 		textAlign: 'center',
@@ -98,3 +98,5 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold'
 	}
 });
+
+export default LeaderBoard;
