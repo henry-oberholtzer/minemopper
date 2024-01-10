e1a6e30a06bcd774f1cb15e56cd0b2ba64f6b419
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-01-10 09:13:25 -0800

    add @react-navigation/native

--- App.js
+++ App.js
@@ -5,23 +5,26 @@ import LeaderBoard from './components/LeaderBoard';
 import MainMenu from './components/MainMenu';
 import Board from './components/Board';
 import newGame from './game_logic/board-creator';
+import { NavigationContainer } from '@react-navigation/native';
 
 const game = newGame(8, 8, 8);
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<MainMenu />
-			<StatusBar style="auto" />
-		</View>
-	);
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <MainMenu />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#C0C0C0',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#C0C0C0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
