31ec6753def8c26f86697f1187285e796b0565c7
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-01-11 11:00:58 -0800

    add save button styling

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import MainMenu from './components/MainMenu';
 import Board from './components/Board';
-import { Button } from 'react-native';
+import { Button, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './components/LoginScreen';
@@ -34,16 +34,27 @@ export default function App() {
           },
           headerTitleAlign: 'center',
           headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              title="Save"
-              color='#9AE190'
-              margin="2"
-            />)
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Save"
+                color='#9AE190'
+                margin="2"
+              />
+            </View>)
         }}
         />
       </Stack.Navigator>
     </NavigationContainer >
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginRight: 8,
+    backgroundColor: '#9AE190',
+    padding: 5,
+    borderRadius: 5,
+  },
+});
 
